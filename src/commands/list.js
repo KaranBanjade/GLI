22e151bd720714a61435b1b99ddecd9e7fa1ca7e
Karan Banjade <karan.banjade@gmail.com>
2022-07-05 12:34:12 +0530

    added objects to export

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,15 +1,22 @@
-const Table = require("cli-table");
-const boilerplates = require("../meta/boilerplate").boilerplates;
-const ObjectStruct = ['id', 'commands', 'preReq', 'website'];
-const table = new Table({
-    head: [...ObjectStruct],
-    colWidths: [20, 20, 20, 60]
-});
+// const Table = require("cli-table");
+// const boilerplates = require("../meta/boilerplate").boilerplates;
+// const ObjectStruct = ['id', 'commands', 'preReq', 'website'];
+// const table = new Table({
+//     head: [...ObjectStruct],
+//     colWidths: [20, 20, 20, 60]
+// });
 
 module.exports = {
     name: "list",
     option: ["help","List all available commands"],
     action: function() {
+        const Table = require("cli-table");
+        const boilerplates = require("../meta/boilerplate").boilerplates;
+        const ObjectStruct = ['id', 'commands', 'preReq', 'website'];
+        const table = new Table({
+            head: [...ObjectStruct],
+            colWidths: [20, 20, 20, 60]
+        });
         boilerplates.map((dat)=>{
             table.push([
                 dat.id,
