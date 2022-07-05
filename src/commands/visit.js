886c8ee507af0d2fec6f5699f99dc21e94b8986d
Karan Banjade <karan.banjade@gmail.com>
2022-07-05 22:42:08 +0530

    multi os fix

--- src/commands/visit.js
+++ src/commands/visit.js
@@ -1,5 +1,6 @@
 const boilerplates = require("../meta/boilerplate").boilerplates;
 const { exec } = require("child_process");
+const os = require('os');
 const output = null;
 module.exports = {
     name: "visit",
@@ -7,8 +8,22 @@ module.exports = {
     argument: [['[string]', 'Framework name']],
     action: function (str, options) {
         if (!str) {
-            console.log("Opening CLI docs")
-            exec("explorer https://google.com")
+            switch(os.type()) {
+                case 'Darwin':
+                    console.log("You are using MacOS\nOpening CLI Docs in Default Browser");
+                    exec("open http://www.google.com");
+                    break;
+                case 'Linux': 
+                    console.log("You are using Linux operating system\nOpening CLI Docs in Default Browser");
+                    exec("xdg-open https://google.com")
+                    break;
+                case 'Windows_NT':
+                    console.log("You are using Windows operating system\nOpening CLI Docs in Default Browser");
+                    exec("explorer https://google.com")
+                    break;    
+                default: 
+                    console.log("other operating system");
+            }
         }
         else if (options.d === true) {
             const boilerplate = boilerplates.find(boilerplate => boilerplate.id === str);
