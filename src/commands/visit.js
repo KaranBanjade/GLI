b131a320a7730771160ece96810cee42660933cc
Karan Banjade <karan.banjade@gmail.com>
2022-07-05 22:57:38 +0530

    added oscheck to all links

--- src/commands/visit.js
+++ src/commands/visit.js
@@ -1,20 +1,21 @@
 const boilerplates = require("../meta/boilerplate").boilerplates;
 const { exec } = require("child_process");
-const output = null;
 const osCheck = require("../helpers/os-visit");
+const output = null;
+const CLIDocsLink = "https://google.com";
 module.exports = {
     name: "visit",
     option: [["-d", "Visit Docs"]],
     argument: [['[string]', 'Framework name']],
     action: function (str, options) {
         if (!str) {
-            osCheck();
+            osCheck(CLIDocsLink);
         }
         else if (options.d === true) {
             const boilerplate = boilerplates.find(boilerplate => boilerplate.id === str);
             if (boilerplate) {
                 console.log("Opening " + boilerplate.id + " docs")
-                exec(`explorer ${boilerplate.docs}`);
+                osCheck(boilerplate.docs);
             }
             else
                 console.log("No such framework");
@@ -23,7 +24,7 @@ module.exports = {
             const boilerplate = boilerplates.find(boilerplate => boilerplate.id === str);
             if (boilerplate) {
                 console.log("Opening " + boilerplate.id + " website")
-                exec(`explorer ${boilerplate.website}`);
+                osCheck(boilerplate.website);
             }
             else
                 console.log("No such framework");
--- src/helpers/os-visit.js
+++ src/helpers/os-visit.js
@@ -1,21 +1,21 @@
-const { exec } = require("child_process");
+const { exec } = require(`child_process`);
 const os = require('os');
-const osCheck = () =>{
+const osCheck = (link) =>{
     switch(os.type()) {
         case 'Darwin':
-            console.log("You are using MacOS\nOpening CLI Docs in Default Browser");
-            exec("open http://www.google.com");
+            console.log(`You are using MacOS\nOpening ${link} in Default Browser`);
+            exec(`open ${link}`);
             break;
         case 'Linux': 
-            console.log("You are using Linux operating system\nOpening CLI Docs in Default Browser");
-            exec("xdg-open https://google.com")
+            console.log(`You are using Linux operating system\nOpening ${link} in Default Browser`);
+            exec(`xdg-open ${link}`)
             break;
         case 'Windows_NT':
-            console.log("You are using Windows operating system\nOpening CLI Docs in Default Browser");
-            exec("explorer https://google.com")
+            console.log(`You are using Windows operating system\nOpening ${link} in Default Browser`);
+            exec(`explorer ${link}`)
             break;    
         default: 
-            console.log("other operating system");
+            console.log(`other operating system`);
     }
 }
 module.exports = osCheck;
