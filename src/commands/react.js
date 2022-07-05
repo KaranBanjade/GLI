b5728f83e506a6511642d685c43bc7eb133def8c
Karan Banjade <karan.banjade@gmail.com>
2022-07-05 18:19:51 +0530

    added angluar to boilerplate

--- src/commands/react.js
+++ src/commands/react.js
@@ -1,7 +1,9 @@
+// this file can be used to look at a boilerplate for how a full fledged command should look like
+// with optional [] and required <> options
 module.exports = {
     name: "react",
     option:[["--start[type]","help"],["--s[type]","help"],["--st[type]","help"]],
-    argument: [['[string]', 'arg',['[string]', 'arg'],['[string]', 'arg']]],
+    argument: [['[string]', 'arg'],['[string]', 'arg'],['[string]', 'arg']],
     action: function(str, options) {
         console.log(Object.keys(options));
     }
