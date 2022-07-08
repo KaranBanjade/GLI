f55f1c34e4e52eda597880d4002757b81cb4c3e7
Karan Banjade <karan.banjade@gmail.com>
2022-07-09 01:28:03 +0530

    added comments

--- src/core/meta.js
+++ src/core/meta.js
@@ -3,6 +3,15 @@ const execute = require("../utils/execute");
 // Actutal execution logic for the desired entity chosen by the user
 module.exports = function(meta, id) {
     let bp = boilerplate.find(bp => bp.id == id);
+            /*
+            actual logic to be implemented: 
+            1. if meta is empty, then replace all the {string} with empty ''
+            2. if meta is not empty check if the {string} in cmd has a pair in meta if not replace with ''
+            3. if meta is not empty check if the {string} in cmd has a pair in meta if yes replace with the value in meta
+
+            problem:
+                1. unable to use regex to check for pattern matching
+            */
     bp.commands.forEach(cmd => {
         Object.keys(meta).forEach(key => {
             if(cmd.includes(`{${key}}`) && meta[key]){   
@@ -14,6 +23,11 @@ module.exports = function(meta, id) {
             console.log(cmd);
             execute(cmd);
         })
+        if(JSON.stringify(meta) === '{}'){
+            cmd = cmd.replace("{name}", '')
+            execute(cmd);
+            return;
+        }
         // // need to remove the placeholder if there is no value
         // let reg = '/{.*}/'
         // cmd = cmd.replace(reg, '')
