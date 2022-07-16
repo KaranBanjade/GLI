97e6fd5f2f50f0afd42f2b57612de8a70b7fcb4f
Karan Banjade <karan.banjade@gmail.com>
2022-07-16 14:15:29 +0530

    added exit

--- src/utils/execute.js
+++ src/utils/execute.js
@@ -3,6 +3,7 @@ const exec = util.promisify(require('child_process').exec);
 const print = require("../utils/print");
 const { createSpinner } = require('nanospinner');
 const { type } = require('os');
+const { exit } = require('process');
 
 // const ex = async(cmd) =>{
 //     await exec(cmd,(err, stdout, stderr,stdio) => {
@@ -19,16 +20,24 @@ const { type } = require('os');
 const ex2 = (cmd) => {
         const ex = exec(cmd);
         const child = ex.child;
-        
+
         child.stdout.on('data', function(data) {
-            console.log('stdout: ' + data);
+        console.log(data );
             child.stdin.write("\n");
+            process.stdout.clearLine();
+            process.stdout.cursorTo(1); 
+            process.stdout.clearLine();
+            process.stdout.cursorTo(1); 
+        if(data.includes("npm run dev")){
+            exit();
+        }
         });
         child.stderr.on('data', function(data) {
             console.log('stderr: ' + data);
         });
         child.on('close', function(code) {
             console.log('closing code: ' + code);
+            child.stdin.write("Press Ctrl+C to exit GLI");
         });
 }
 const execute = async (cmd, loaderText) => {
@@ -38,7 +47,7 @@ const execute = async (cmd, loaderText) => {
         if (loaderText) {
             spinner = createSpinner(loaderText).start()
         }
-        await ex2(cmd);
+         ex2(cmd);
         if (spinner) spinner.success()
         return true;
     } catch (e) {
