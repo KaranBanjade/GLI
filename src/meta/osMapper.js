253fc149051a6ab7441fe810d4870a46f2ee125d
Karan Banjade <karan.banjade@gmail.com>
2023-07-18 16:57:57 +0530

    add-feature: install requirements

--- src/meta/osMapper.js
+++ src/meta/osMapper.js
@@ -4,16 +4,37 @@ module.exports = Object.freeze({
             id: "Darwin",
             os: "MacOS",
             commands: { LINK: "open" },
+            installers: {
+                "node": `curl -fsSL https://webinstall.dev/node@stable | bash`,
+                "npm" : `npm install -g npm | bash`,
+                "ng" : `npm install -g @angular/cli | bash`,
+                "go": `curl -fsSL https://webinstall.dev/golang@1.14 | bash`,
+                "cargo": `curl https://sh.rustup.rs -sSf | sh`,
+            }
         },
         {
             id: "Linux",
             os: "Linux",
             commands: { LINK: "xdg-open" },
+            installers: {
+                "Node" : `curl -fsSL https://webinstall.dev/node@stable | bash`,
+                "npm" : `npm install -g npm | bash`,
+                "ng" : `npm install -g @angular/cli | bash`,
+                "go" : `curl -fsSL https://webinstall.dev/golang@1.14 | bash`,
+                "cargo" : `curl https://sh.rustup.rs -sSf | sh`,
+            }
         },
         {
             id: "Windows_NT",
             os: "Windows",
-            commands: { LINK: "explorer" }
+            commands: { LINK: "explorer" },
+            installers: {
+                "Node" : `curl.exe -fsSA "MS" https://webinstall.dev/node@stable | powershell`,
+                "npm" : `npm install -g npm`,
+                "ng" : `npm install -g @angular/cli`,
+                "go" : `curl.exe -fsSA "MS" https://webinstall.dev/golang@1.14 | powershell`,
+                "cargo": `curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | powershell`,
+            }
         }
     ]
 });
