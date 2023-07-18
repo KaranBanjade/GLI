6a9d1fa95e52b6c7d5fa54996e7c56e0e18422fc
Karan Banjade <karan.banjade@gmail.com>
2023-07-18 19:45:16 +0530

    revert

--- src/core/meta.js
+++ src/core/meta.js
@@ -1,8 +1,6 @@
 const { boilerplates, requirements } = require("../meta/boilerplate");
-const {osMap} = require("../meta/osMapper");
 const execute = require("../utils/execute");
 const print = require("../utils/print");
-const os = require('os');
 // Actutal execution logic for the desired entity chosen by the user
 module.exports = async function (meta, id) {
     let bp = boilerplates.find(bp => bp.id == id);
@@ -28,34 +26,11 @@ module.exports = async function (meta, id) {
 }
 
 const checkRequirements = async (commandRequirements, coreRequirements) => {
-    const OS =  osMap.find(OS => OS.id == os.type());
     for (index = 0; index < commandRequirements.length; index++) {
         try {
             let req = commandRequirements[index];
             let cmdSuccess = await execute(coreRequirements[req], `Checking for ${req}`);
-            if(!cmdSuccess) {
-                print(`Installing ${req}`, "yellow");
-                cmdSuccess = await requirementsInstaller([req]);
-            };
             if (!cmdSuccess) return cmdSuccess;
-        } catch (err) {
-            return false;
-        }
-    };
-    return true;
-}
-
-const requirementsInstaller = async (requirements) => {
-        const OS =  osMap.find(OS => OS.id == os.type());
-        for (index = 0; index < requirements.length; index++) {
-        try {
-            let req = requirements[index];
-            let cmdSuccess = await execute(OS.installers[req], `Installing ${req}`);
-            if(!cmdSuccess) return cmdSuccess;
-            else {
-                print(`Installed ${req}`, "green");
-                return true;
-            }
         } catch (err) {
             return false;
         }
