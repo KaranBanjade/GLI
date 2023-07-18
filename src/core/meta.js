083097db3b6bde562a6ddfc95f339ef0c3b61273
Karan Banjade <karan.banjade@gmail.com>
2023-07-19 00:59:16 +0530

    removed: buggy code

--- src/core/meta.js
+++ src/core/meta.js
@@ -1,11 +1,7 @@
 const { boilerplates, requirements } = require("../meta/boilerplate");
-const { osMap } = require("../meta/osMapper");
 const execute = require("../utils/execute");
 const print = require("../utils/print");
-const os = require('os');
-const readline = require('readline');
-const { exit } = require("process");
-
+// Actutal execution logic for the desired entity chosen by the user
 module.exports = async function (meta, id) {
     let bp = boilerplates.find(bp => bp.id == id);
     let isReqMet = await checkRequirements(bp.requirements, requirements);
@@ -22,72 +18,20 @@ module.exports = async function (meta, id) {
             let cmdSuccess = await execute(cmd, `Initializing ${bp.id}`);
             init = init && cmdSuccess;
         };
-
-        if (init) {
-            print(`Initialized ${bp.id}`, "green");
-            exit(0);
-        } else {
-            print(`Failed to initialize ${bp.id}`, "red");
-        }
-    } else {
+        if(init) print(`Initialized ${bp.id}`, "green");
+    }
+    else {
         print("Requirements not met", "red");
     }
 }
 
-
-// to create streams for reading user input
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
 const checkRequirements = async (commandRequirements, coreRequirements) => {
-    const OS = osMap.find(OS => OS.id == os.type());
-
-    async function processRequirement(index) {
-        if (index >= commandRequirements.length) return true;
-
-        const req = commandRequirements[index];
-        let cmdSuccess = await execute(coreRequirements[req], `Checking for ${req}`);
-
-        // promp generation if requirement not met
-        if (!cmdSuccess) {
-            print(`${req} not found`, "blue");
-
-            await new Promise(resolve => {
-                rl.question(`Do you want to install ${req}? (y/n) `, async (resp) => {
-                    if (resp.toLowerCase() === 'y') {
-                        // install requirement
-                        cmdSuccess = await requirementsInstaller([req]);
-                    }
-                    resolve(); 
-                });
-            });
-        }
-
-
-        if (!cmdSuccess) return cmdSuccess;
-        // if requirement is met, check for next requirement
-        return processRequirement(index + 1); 
-    }
-
-    return processRequirement(0); 
-};
-
-
-const requirementsInstaller = async (requirements) => {
-    const OS = osMap.find(OS => OS.id == os.type());
-    for (index = 0; index < requirements.length; index++) {
+    for (index = 0; index < commandRequirements.length; index++) {
         try {
-            let req = requirements[index];
-            let cmdSuccess = await execute(OS.installers[req], `Installing ${req}`);
+            let req = commandRequirements[index];
+            let cmdSuccess = await execute(coreRequirements[req], `Checking for ${req}`);
             if (!cmdSuccess) return cmdSuccess;
-            else {
-                print(`Installed ${req}`, "green");
-                return true;
-            }
         } catch (err) {
-            print(`Failed to install ${req}`, "red");
             return false;
         }
     };
